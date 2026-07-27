c089bde4ad0fe0d63b8c4430bfea8ee6cb6e3ecc
Debora Petry <deborapetry@gmail.com>
2026-07-27 20:36:53 -0300

    New scopes for google home apis (test)

--- server/server.js
+++ server/server.js
@@ -70,21 +70,28 @@ async function getValidAccessToken() {
 }
 
 // Endpoint para iniciar login OAuth
-
 app.get("/auth/google", (req, res) => {
-  /*  const authUrl =
-    `https://accounts.google.com/o/oauth2/v2/auth?` +
-    `client_id=${process.env.GOOGLE_CLIENT_ID}&` +
-    `redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI)}&` +
-    `response_type=code&` +
-    `scope=https://www.googleapis.com/auth/calendar.readonly` +
-    `access_type=offline&` +
-    `prompt=consent`; */
-  const params = new URLSearchParams({
+  /*const params = new URLSearchParams({
     client_id: process.env.GOOGLE_CLIENT_ID,
     redirect_uri: process.env.REDIRECT_URI,
     response_type: "code",
     scope: "https://www.googleapis.com/auth/calendar.readonly",
+    access_type: "offline",
+    prompt: "consent",
+  });
+
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?${params}`; */
+  const scopes = [
+    "https://www.googleapis.com/auth/calendar.readonly",
+    "https://www.googleapis.com/auth/home.platform.v2",
+    "https://www.googleapis.com/auth/home.platform.selected.devices",
+  ];
+
+  const params = new URLSearchParams({
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: process.env.REDIRECT_URI,
+    response_type: "code",
+    scope: scopes.join(" "),
     access_type: "offline",
     prompt: "consent",
   });

4c04d545ae13b459700ba3040c9f8af2d3216537
Debora Petry <deborapetry@gmail.com>
2026-07-28 12:48:31 -0300

    again

--- server/server.js
+++ server/server.js
@@ -83,25 +83,6 @@ app.get("/auth/google", (req, res) => {
 
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
 
-    /*  const scopes = [
-    "https://www.googleapis.com/auth/calendar.readonly",
-    "https://www.googleapis.com/auth/home.platform.v2",
-    "https://www.googleapis.com/auth/home.platform.selected.devices",
-  ];
-
-  console.log("Tentando gerar URL de login com scopes:", scopes.join(" "));
-
-  try {
-    const params = new URLSearchParams({
-      client_id: process.env.GOOGLE_CLIENT_ID,
-      redirect_uri: process.env.REDIRECT_URI,
-      response_type: "code",
-      scope: scopes.join(" "),
-      access_type: "offline",
-      prompt: "consent",
-    }); */
-
-    //const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
     console.log("URL gerada com sucesso");
     res.json({ authUrl });
   } catch (err) {
